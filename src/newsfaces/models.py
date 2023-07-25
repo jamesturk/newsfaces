79c6a45718f7dc36b0f7602417411cf13d336ed1
James Turk <dev@jamesturk.net>
2023-07-25 12:06:58 -0500

    add initial models.py

---
+++ src/newsfaces/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+class ArticleURL(BaseModel):
+    url: str
+    source: str
+
+
+class ImageType(Enum):
+    main = "main"
+    social = "social"
+    video_thumbnail = "video_thumbnail"
+
+
+class Image(BaseModel):
+    url: str
+    image_type: ImageType
+    caption: str = ""
+    alt_text: str = ""
+
+
+class Article(BaseModel):
+    title: str
+    article_text: str
+    images: list[Image] = []
