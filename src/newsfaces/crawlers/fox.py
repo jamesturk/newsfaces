c6ee144a0f8fd7d6059d1b93a1abb40a80ce6d7f
Katherine Dumais <kdumais@uchicago.edu>
2023-07-27 14:48:26 -0500

    merge conflict resolved

--- src/newsfaces/crawlers/fox.py
+++ src/newsfaces/crawlers/fox.py
@@ -1,6 +1,6 @@
 # Util Functions
-from newsfaces.utils import make_link_absolute, page_grab
-from crawler import Crawler, WaybackCrawler
+from crawlers.utils import make_link_absolute, page_grab
+from crawlers.crawler import Crawler, WaybackCrawler
 import json
 
 
