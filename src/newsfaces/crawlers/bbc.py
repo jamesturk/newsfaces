e44d514c3725c7f0f3466c3afcc1e4f84eedccec
Katherine Dumais <kdumais@uchicago.edu>
2023-07-31 23:11:56 -0500

    shift to xpath

--- src/newsfaces/crawlers/bbc.py
+++ src/newsfaces/crawlers/bbc.py
@@ -72,6 +72,16 @@ class BBC(WaybackCrawler):
     def get_archive_urls(self, url, selector):
         return self.get(url)
 
+
+class BBC(WaybackCrawler):
+    def __init__(self):
+        super().__init__()
+        self.start_url = "https://www.bbc.com/news/topics/cwnpxwzd269t"
+        self.selector = ["div.archive__item__content", "h2.node__title.node-title"]
+
+    def get_archive_urls(self, url, selector):
+        return self.get(url)
+
     def get(self, url, articles=set(), videos=set()):
         """
         This function takes a URLs and returns lists of URLs
@@ -84,21 +94,17 @@ class BBC(WaybackCrawler):
             A list of URLs to each video and article on that page.
         """
         response = self.make_request(url)
-        container = response.cssselect("div")
-        filtered_container = [
-            elem for elem in container if elem.get("type") is not None
-        ]
-
-        for j in filtered_container:
-            # find video/article
-            type = j.get("type")
-            # find link
-            if type == "article" or type == "video":
-                a = j[0].cssselect("a")
-                href = a[0].get("href")
-                href = make_link_absolute(href, " https://web.archive.org")
-            if type == "article":
-                articles.add(href)
-            elif type == "video":
-                videos.add(href)
+        xpath_sel= ['article','video']
+            # for items that have random characters continually added at the end so we do non-exact matching
+        for j in xpath_sel:
+            container = response.xpath(f"//div[contains(@type, '{j}')]")
+            if container:
+                for j in container:
+                    a = j[0].cssselect("a")
+                    href = a[0].get("href")
+                    href = make_link_absolute(href, "https://web.archive.org")
+                    if j == "article":
+                        articles.add(href)
+                    else:
+                        videos.add(href)
         return articles.union(videos)
