f9e94e463bb5b4369175c19770ef3d679da35dda
José Pablo Martínez Claeys <jpmartinezclaeys@Joses-MacBook-Air.local>
2023-08-03 16:52:54 -0500

    Changes to Extract Class

--- src/newsfaces/extract_html.py
+++ src/newsfaces/extract_html.py
@@ -4,8 +4,8 @@ from newsfaces.models import Article, Image, ImageType
 
 
 class Extractor(object):
-    def __init__(self):
-        self.html = ""
+    def __init__(self, html):
+        self.html = html
         self.article_body = []
         self.img_p_selector = []
         self.img_selector = ["img"]
@@ -41,8 +41,8 @@ class Extractor(object):
             if len(self.html.cssselect(selector)[0]) > 0:
                 article_body = self.html.cssselect(selector)[0]
                 break
-        if self.head_img_select:
-            imgs += self.extract_head_img(
+        if self.head_img_div:
+            imgs += self.extract_imgs(
                 self.html, self.head_img_div, self.head_img_select
             )
         imgs += self.extract_imgs(article_body, self.img_p_selector, self.img_selector)
@@ -52,58 +52,6 @@ class Extractor(object):
                 t_text = self.html.cssselect(t)[0].text
                 break
         return imgs, art_text, t_text
-
-def extract_html(
-    html, article_selector,img_p_selector, img_selector="img", p_selector=None,
-    t_selector=None):
-    """
-    Extract the image and text content from and HTML:
-    Inputs:
-        - html(str): Full html of an artcile url
-        - article_selector(str): css selector for article container
-        - img_p_selector(str): css selector for the parent elements of images in article
-        - img_selector(str): css selector for images living inside the article
-        container
-        - p_selector(str): css selector for paragraphs living inside the article container
-        - t_selector(str): css selector for title living inside the article container
-    Return:
-        -imgs(lst): list where each element is an image represented as a dictionary
-        with src, alt, title, and caption as fields
-        - art_text(str): Article text
-        - t_text(str): Title
-    """
-    article_body = html.cssselect(article_selector)[0]
-    imgs = extract_imgs(article_body,img_p_selector,img_selector)
-    art_text = extract_text(article_body, p_selector)
-    if t_selector:
-        t_text = html.cssselect(t_selector)[0].text
-    return imgs, art_text, t_text
-
-def extract_imgs(html, img_p_selector,img_selector="img"):
-    """
-    Extract the image content from an HTML:
-    Inputs:
-        - html(str): html to extract images from
-        - img_p_selector(list): list of css selector for the parent elements of images in articles
-        - img_selector(str): css selector for the image elements
-        Return:
-        -imgs(lst): list where each element is an image represented as a dictionary
-        with src, alt, title, and caption as fields
-    """
-    imgs = []
-    for selector in img_p_selector:
-        img_container = html.cssselect(selector)
-        for container in img_container:
-            images = container.cssselect(img_selector)
-            for img in images:
-                img_item = {}
-                img_item["src"] = img.get("src")
-                img_item["alt"] = img.get("alt")
-                img_item["title"] = img.get("title")
-                img_item["caption"] = img.get("caption")
-                imgs.append(img_item)
-    return imgs
-
 
     def extract_text(self, html, p_selector):
         """
@@ -124,7 +72,7 @@ def extract_imgs(html, img_p_selector,img_selector="img"):
 
         return text
 
-    def extract_head_img(self, html, img_p_selector, img_selector):
+    def extract_imgs(self, html, img_p_selector, img_selector):
         """
         Extract the image content from an HTML:
         Inputs:
@@ -135,6 +83,7 @@ def extract_imgs(html, img_p_selector,img_selector="img"):
             -imgs(lst): list where each element is an image represented as a dictionary
             with src, alt, title, and caption as fields
         """
+        imgs = []
         for selector in img_p_selector:
             img_container = html.cssselect(selector)
             if len(img_container) == 0:
@@ -149,32 +98,11 @@ def extract_imgs(html, img_p_selector,img_selector="img"):
                             caption=i.get("caption") or "",
                             alt_text=i.get("alt") or "",
                         )
+                        imgs.append(img_item)
                     break
-        return [img_item]
+        return imgs
 
     def scrape(self):
         imgs, art_text, t_text = self.extract_html()
         article = Article(title=t_text or "", article_text=art_text or "", images=imgs)
         return article
-
-
-class Fox_Extractor(Extractor):
-    def __init__(self):
-        super().__init__()
-        self.html = page_grab(
-            "https://www.foxnews.com/politics/kerry-ripped-demanding-agriculture-emission-cuts-bankrupt-every-farmer"
-        )
-        self.article_body = ["div.article-content-wrap.sticky-columns"]
-        self.img_p_selector = ["div.m"]
-        self.img_selector = ["img"]
-        self.head_img_div = ["div.contain"]
-        self.head_img_select = ["img"]
-        self.p_selector = ["p"]
-        self.t_selector = ["h1", "h6"]
-
-    def extract_head_img(self, html="", img_p_selector="", img_selector=""):
-        return []
-
-
-a = Fox_Extractor()
-a.scrape()
