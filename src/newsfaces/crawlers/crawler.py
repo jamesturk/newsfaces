1c9b4da01d4c07d894dc72e2b1399ad2e1de82ac
James Turk <dev@jamesturk.net>
2023-08-08 15:03:59 -0500

    fix ruff error

--- src/newsfaces/crawlers/crawler.py
+++ src/newsfaces/crawlers/crawler.py
@@ -81,7 +81,8 @@ class WaybackCrawler(Crawler):
         start_date(datetime object): Earliest day for which to look for results
         end_date(datetime object): Latest day for which to look for results
         delta_hrs (int): Threshold of minimum number of hours between the
-        timestamp of consecutive internet archive results for which to look for article urls.
+        timestamp of consecutive internet archive results for which to look for
+        article urls.
         """
         super().__init__()
         self.session = WaybackSession()
