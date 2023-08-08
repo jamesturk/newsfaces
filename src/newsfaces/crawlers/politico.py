b8d805ee2c1b0244fcdd132342f639f33bfb3101
kdumais111 <kdumais@uchicago.edu>
2023-08-08 14:54:53 -0500

    rev politico

--- src/newsfaces/crawlers/politico.py
+++ src/newsfaces/crawlers/politico.py
@@ -1,10 +1,8 @@
-
 # Util Functions
 from newsfaces.extract_html import Extractor
 from newsfaces.utils import make_link_absolute, page_grab
 from newsfaces.crawlers.crawler import Crawler
 from newsfaces.models import Image, Article, ImageType
-
 
 
 class Politico(Crawler):
@@ -39,26 +37,28 @@ class Politico(Crawler):
                 urls.append(make_link_absolute(href, "https://www.politico.com"))
         return urls
 
-    def recurse_politico(self, url, breakpoint, urls=[]):
+    def crawl_politico(self, url, breakpoint, urls=[]):
         """
         Runs get_url's function on all possible article landing pages
         until a specific page (breakpoint). Returns a list of article
         urls across pages
         """
-        scraped_urls = self.get_urls(url)
-        urls += scraped_urls
+        urls = self.get_urls(url)
         begin = url.find("politics/") + 9
         pagenumber = int(url[begin : len(url)])
         if pagenumber < breakpoint:
             newlink = url[: -len(str(pagenumber))] + str(pagenumber + 1)
-            self.recurse_politico(newlink, breakpoint, urls)
-        return urls
+        else:
+            newlink = None
+        return urls, newlink
 
     def politico_get_urls(self):
-        urllist = self.recurse_politico("https://www.politico.com/politics/1", 1700)
-        urllist2 = self.recurse_politico("https://www.politico.com/politics/1700", 3400)
-        pooled = set(urllist + urllist2)
-        return pooled
+        newlink = "https://www.politico.com/politics/1"
+        urls = set()
+        while newlink is not None:
+            urllist, newlink = self.crawl_politico(newlink, 10)
+            urls = urls.union(set(urllist))
+        return urls
 
 
 class Politico_Extractor(Extractor):
@@ -90,14 +90,14 @@ class Politico_Extractor(Extractor):
     def extract_video_imgs(self, html):
         videos = []
         imgs = []
-        print(html)
+
         for i in self.video:
             videos += html.cssselect(i)
+            item = []
+            cap_elements = []
             for v in videos:
-                item = v.cssselect("video")
-                v.xpath('//div[contains(@class, "vjs-dock-text")]')
-
-            cap_elements = v.xpath('//div[contains(@class, "vjs-dock-text")]')
+                item += v.cssselect("video")
+                cap_elements += v.xpath('//div[contains(@class, "vjs-dock-text")]')
 
             # Extract captions from cap_elements
             captions = [element.text_content() for element in cap_elements]
@@ -110,13 +110,10 @@ class Politico_Extractor(Extractor):
                     alt_text="",
                 )
                 imgs.append(img_item)
-
-        return imgs
-
-    def extract_head_img(self, html, img_p_selector, img_selector):
-        return []
+            return imgs
 
 
+# tests
 a = Politico_Extractor()
 a.scrape(
     "https://www.politico.com/news/2023/08/07/bidenomics-white-house-economy-00109977"
