fdd12d05d60d8ba4048e3b190b2359aa1778a03b
James Turk <dev@jamesturk.net>
2023-08-15 23:26:32 -0500

    bbc yielding, #47

--- src/newsfaces/crawlers/bbc.py
+++ src/newsfaces/crawlers/bbc.py
@@ -1,37 +1,28 @@
 from ..crawler import Crawler, WaybackCrawler
 from newsfaces.utils import make_link_absolute
+from newsfaces.models import URL
 
 
 class BBC_Latest(Crawler):
     def __init__(self):
         super().__init__()
         self.start_url = "https://www.bbc.com/news/topics/cwnpxwzd269t?page=1"
+        self.source = "bbc_latest"
 
     def crawl(self):
         """
         run get_html with correct initial html from init
         """
-        return self.get_newslink(self.start_url)[0]
+        url = self.start_url
+        pagenumber = 0
+        while pagenumber < 42:
+            yield from self.get_urls(url)
+            pagenumber += 1
+            # TODO: why 42?
+            if pagenumber < 42:
+                url = url[: -len(str(pagenumber))] + str(pagenumber + 1)
 
-    def get_newslink(self, url, articles=set(), videos=set()):
-        """
-        Takes an initial url and runs get_urls on all possible
-        API queries. Gathering all possible articles and videos
-        from the API into a set.
-        """
-        article, video = self.get_urls(url)
-        articles = articles.union(article)
-        videos = videos.union(video)
-        begin = url.find("page=") + 5
-        pagenumber = int(url[begin : len(url)])
-        if pagenumber < 42:
-            newlink = url[: -len(str(pagenumber))] + str(pagenumber + 1)
-            article, video = self.get_newslink(newlink, articles, videos)
-            articles = articles.union(article)
-            videos = videos.union(video)
-        return articles, videos
-
-    def get_urls(self, url, articles=set(), videos=set()):
+    def get_urls(self, url):
         """
         This function takes a URLs and returns lists of URLs
         for containing each article and video on that page.
@@ -57,10 +48,9 @@ class BBC_Latest(Crawler):
                 href = a[0].get("href")
                 href = make_link_absolute(href, "https://www.bbc.com")
             if type == "article":
-                articles.add(href)
+                yield URL(url=href, source=self.source)
             elif type == "video":
-                videos.add(href)
-        return articles, videos
+                pass  # TODO: video
 
 
 class BBC(WaybackCrawler):
