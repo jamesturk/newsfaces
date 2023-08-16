f1c2d74e4d0bbec2fccbd7817e4eac53622cdd94
James Turk <dev@jamesturk.net>
2023-08-15 23:17:39 -0500

    daily yielding, #55

--- src/newsfaces/crawlers/daily.py
+++ src/newsfaces/crawlers/daily.py
@@ -1,5 +1,6 @@
 import re
 import datetime
+from newsfaces.models import URL
 from ..crawler import Crawler
 from ..utils import make_link_absolute
 
@@ -9,21 +10,22 @@ class DailyCrawler(Crawler):
         super().__init__()
         self.url = "https://dailycaller.com/section/politics/"
         self.prefix = "https://dailycaller.com/"
+        self.source = "daily"
 
     def obtain_page_urls(self, page="1"):
         """
         Obtain the urls of the politics section of a url in the Daily Caller politics
         section
+
         Inputs:
-        - page(str): page of the politics section to fetch urls
-        Return:
-        - articles_set (set): Set of unique urls
-        - year (int): year of last article fetched on the page
+            - page(str): page of the politics section to fetch urls
+        Yields:
+            - url(URl): url of the article
+            - year(str): year of the article
         """
         url = self.url + "page/{}/".format(page)
         root = self.make_request(url)
         article_elements = root.cssselect("article.relative")
-        article_list = []
         for article in article_elements:
             link = article.cssselect("a")[0].get("href")
             # Some articles in the Daily Caller politcs section
@@ -31,27 +33,8 @@ class DailyCrawler(Crawler):
             if link.startswith("http://checkyourfact"):
                 continue
             full_link = make_link_absolute(link, self.prefix)
-            article_list.append(full_link)
-
-        year = re.search(r"\d{4}", article_list[-1]).group()
-        print("Year:", year)
-        articles_set = set(article_list)
-
-        return articles_set, int(year)
-
-    def obtain_year_urls(self, page=1, year=2023):
-        """
-        Obtain the results of
-        """
-        links_set = set()
-        article_year = 2023
-        while article_year >= year:
-            print("Obtaining results for page", page)
-            page_links, article_year = self.obtain_page_urls(str(page))
-            links_set.update(page_links)
-            page += 1
-
-        return links_set, page
+            year = re.search(r"\d{4}", full_link).group()
+            yield URL(url=full_link, source=self.source), int(year)
 
     def crawl(self, start_date=datetime.date(2015, 1, 1)):
         """
@@ -60,11 +43,11 @@ class DailyCrawler(Crawler):
         min_year = int(start_date.strftime("%Y"))
         years = [*range(min_year, 2024, 1)]
         page = 1
-        articles_set = set()
         for year in reversed(years):
-            print("Obtainings links for", year)
-            year_set, page = self.obtain_year_urls(page, year)
-            articles_set.update(year_set)
-            page += 1
-
-        return articles_set
+            print("Obtaining links for", year)
+            article_year = 2023
+            while article_year >= year:
+                print("Obtaining results for page", page)
+                for url, year in self.obtain_page_urls(str(page)):
+                    yield url
+                page += 1
