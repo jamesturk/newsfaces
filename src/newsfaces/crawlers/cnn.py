392d61994a18935ee6841cfc9ecc58397dba51e0
James Turk <dev@jamesturk.net>
2023-08-17 17:31:51 -0500

    CNN wip

--- src/newsfaces/crawlers/cnn.py
+++ src/newsfaces/crawlers/cnn.py
@@ -1,10 +1,12 @@
-from .crawler import WaybackCrawler, START_DATE, END_DATE, DELTA_HRS
-from wayback import memento_url_data
+from ..crawler import WaybackCrawler
 from ..utils import make_link_absolute
+from databeakers.http import HttpResponse
+from ..models import URL
 import lxml.html
 import requests
 import datetime
 import pytz
+from typing import Generator
 
 # The results for https://cnn.com/politics have different types of answers
 # depending on the date so we need to create additional datetime objects to deal
@@ -59,72 +61,6 @@ class CnnArchive(WaybackCrawler):
 
         return urls
 
-    def crawl(self, start_date=START_DATE, end_date=END_DATE, delta_hrs=DELTA_HRS):
-        """
-        Crawl to obtain all the urls of articles contained in start_url in the different
-        stored versions in the internet archive between two dates.
-
-        Inputs:
-        - start_date(datetime object): Earliest day for which to look for results
-        - end_date(datetime object): Latest day for which to look for results
-        -delta_hrs (int): Threshold of minimum number of hours between the
-                        timestamp of consecutive internet archive results
-                        for which to look for article urls.
-        Return:
-        - post_date_articles(set): Set of urls obtained from the crawling process
-        """
-        post_date_articles = set()
-
-        # Get first result
-        current_date = start_date
-        results = self.client.search(
-            self.start_url, match_type="exact", from_date=current_date
-        )
-        record = next(results)
-
-        # Crawl internet archive in gaps of at least delta_hrs
-        while current_date < end_date:
-            # Check if timestamp is a time where specific function for a
-            # cnn archived webpage that uses java script
-            if JS_START_DATE <= current_date <= JS_END_DATE:
-                articles = self.get_archive_urls_js(record)
-            else:
-                waybackurl = record.view_url
-                articles = self.get_archive_urls_njs(waybackurl, self.selector)
-                articles = [
-                    memento_url_data(item)[0]
-                    if ("/web/" in item or "web.archive.org" in item)
-                    else item
-                    for item in articles
-                ]
-
-            post_date_articles.update(articles)
-
-            # Continue only if it's possible to visit another result
-            try:
-                next_result = next(results)
-            except StopIteration:
-                break
-            next_time = next_result.timestamp
-
-            # Check time conditions for CNN
-            # Avoid visiting archived cnn pages that not work
-            if STOP_WORKING_DATE <= next_time <= JS_START_DATE:
-                current_date = JS_START_DATE
-                results = self.client.search(
-                    self.start_url, match_type="exact", from_date=current_date
-                )
-                record = next(results)
-            # If gap between fetched and next result is less than delta_hrs,
-            # search the archive for the first results in at least delta_hrs
-            elif next_time - current_date < datetime.timedelta(hours=delta_hrs):
-                current_date += datetime.timedelta(hours=delta_hrs)
-                results = self.client.search(
-                    self.start_url, match_type="exact", from_date=current_date
-                )
-                record = next(results)
-            else:
-                current_date = next_time
-                record = next_result
-
-        return post_date_articles
+    def get_article_urls(self, response: HttpResponse) -> Generator[URL, None, None]:
+        print(response.url)
+        # breakpoint()
