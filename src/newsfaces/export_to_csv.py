a14786fadeeaa672704dd0c6927fd255fdffe970
James Turk <dev@jamesturk.net>
2023-08-25 19:55:21 +0000

    initial export_to_csv

---
+++ src/newsfaces/export_to_csv.py
@@ -0,0 +1,18 @@
+import csv
+from newsfaces.pipeline import pipeline
+
+with open("images.csv", "w") as f:
+    fields = ["article_id", "article_title", "image_url", "image_caption", "image_alt_text", "image_type"]
+    dw = csv.DictWriter(f, fields)
+
+    for row in pipeline._grab_rows(["article"], max_items=100000, offset=0):
+        for img in row["images"]:
+            dw.writerow(
+                    dict(
+                    article_id=row["id"], 
+                    article_title=row["title"], 
+                    image_url=img['url'],
+                    image_caption=img['caption'],
+                    image_alt_text=img['alt_text'],
+                    image_type=str(img['image_type'])
+                ))
