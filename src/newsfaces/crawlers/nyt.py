27bfdf340db1079f190e8cf1f9aebe23d9031b04
JPMartinez <jpmartinez@uchicago.edu>
2023-08-28 11:08:37 -0400

    Changes to NYT Class

--- src/newsfaces/crawlers/nyt.py
+++ src/newsfaces/crawlers/nyt.py
@@ -1,98 +1,23 @@
 from ..crawler import Crawler, START_DATE, END_DATE
 from ..utils import make_link_absolute
+from ..models import Article, Image,ImageType
 import json
 import requests
 import time
+import re
 
 nyt_api_key = ""
-POLITICS_SECTION = "politics"
-QUERY_LIST = [
-    "Donald Trump",
-    "Joe Biden",
-]  # Add default names of politicians to look for
-# if we go with search approach
-FILTER_LIST = ["headline", "lead_paragraph"]
-
+POLITICS_SECTION = '"politics"'
+SLEEP_TIME = 12
 
 class NYTCrawler(Crawler):
     def __init__(self):
         super().__init__()
         self.start_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
 
-    def create_url(
-        self,
-        tags=None,
-        news_desk=POLITICS_SECTION,
-        section="Washington",
-        filters=FILTER_LIST,
-        begin_date=START_DATE,
-        end_date=END_DATE,
-        page="0",
-    ):
-        """
-        Create request url for API based on query search parameters passed to the
-            function.
-
-        Inputs:
-            tags (lst): list of tags (strings) to look for. The tags to filter for
-                are looked in the filters defined in the "filters" parameter.
-            filters (lst): list of filters where to look tags. They can be "headline",
-                "lead_paragraph" and/or "body"
-            begin_date (str): 8 digits (YYYYMMDD) string that specify the begin date
-                or from when to start looking for articles.
-            end_date (str): 8 digits (YYYYMMDD) string that specify the end date or
-                until when to stop looking for articles.
-            page (str): number of page string that states where to look for articles.
-
-        Return (str): URL string with query to send request to NYT Article Search
-            API
-        """
-        # Convert datetime object to str to pass it into API
-        begin_date = begin_date.strftime("%Y%m%d")
-        end_date = end_date.strftime("%Y%m%d")
-
-        filters_copy = filters[:]
-
-        # Create URL
-        if tags:
-            tags_copy = tags[:]
-            filters_copy = filters[:]
-            for i, tag in enumerate(tags_copy):
-                tags_copy[i] = '"' + tag + '"'
-            for i, fil in enumerate(filters_copy):
-                filters_copy[i] = fil + ":(" + " OR ".join(tags_copy) + ")"
-            fq = (
-                "fq="
-                + " OR ".join(filters_copy)
-                + " AND news_desk="
-                + news_desk
-                + " AND section="
-                + section
-            )
-        else:
-            fq = "fq=" + "news_desk=" + news_desk + "AND section=" + section
-        url = (
-            self.start_url
-            + fq
-            + "&begin_date="
-            + begin_date
-            + "&end_date="
-            + end_date
-            + "&page="
-            + page
-            + "&sort=oldest"
-            + "&api-key="
-            + nyt_api_key
-        )
-
-        return url
-
     def make_request(
         self,
-        tags=None,
         news_desk=POLITICS_SECTION,
-        section="Washington",
-        filters=FILTER_LIST,
         begin_date=START_DATE,
         end_date=END_DATE,
         page="0",
@@ -115,37 +40,27 @@ class NYTCrawler(Crawler):
         Return (Response): API request response with specified query parameters
         """
 
-        url = self.create_url(
-            tags, news_desk, section, filters, begin_date, end_date, page
-        )
-        resp = requests.get(url)
+        begin_date = begin_date.strftime("%Y%m%d")
+        end_date = end_date.strftime("%Y%m%d")
+        fq = "news_desk:(" + news_desk + ")"
+
+        parameters = {
+            "fq" : fq,
+            "begin_date" : begin_date,
+            "end_date" : end_date,
+            "sort" : "oldest",
+            "page" : page,
+            "api-key":nyt_api_key
+        }
+
+        resp = requests.get(self.start_url,params=parameters)
         resp_json = json.loads(resp.text)
 
         return resp_json
 
-    def obtains_articles_info(self, resp_json):
-        """
-        Retrieves only the articles information of the JSON response and converts all
-        multimedia relative src urls to a full link.
-        Inputs:
-        - resp_json (json string): Full json string response from the NYT API call
-        Returns:
-        - Articles (json string): Obtains the article information of the json response
-        """
-        articles = resp_json["response"]["docs"]
-        for article in articles:
-            multimedia = article["multimedia"]
-            for item in multimedia:
-                url = "/" + item["url"]
-                item["url"] = make_link_absolute(url, "https://www.nytimes.com/images/")
-        return articles
-
     def crawl(
         self,
-        tags=None,
         news_desk=POLITICS_SECTION,
-        section="Washington",
-        filters=FILTER_LIST,
         begin_date=START_DATE,
         end_date=END_DATE,
     ):
@@ -156,10 +71,9 @@ class NYTCrawler(Crawler):
         page_responses = []
 
         resp = self.make_request(
-            tags, news_desk, section, filters, begin_date, end_date
+            news_desk, begin_date, end_date
         )
-        articles = self.obtain_articles_info(resp)
-        page_responses.append(articles)
+        page_responses.append(resp)
         # Get number of articles that match our query search parameters
         hits = resp["response"]["meta"]["hits"]
 
@@ -168,13 +82,55 @@ class NYTCrawler(Crawler):
 
         for page_n in range(1, max_pages + 1):
             # Limit of 5 requests per minute
-            time.sleep(12)
-            resp = self.make_request(page=str(page_n))
+            print("Obtaining results for page",page_n,"/",max_pages)
+            time.sleep(SLEEP_TIME)
+            resp = self.make_request(news_desk,begin_date,end_date,page=str(page_n))
             status = resp.get("status")
+            page_responses.append(resp)
 
-            if status == "OK":
-                articles = self.obtain_articles_info(resp)
-                page_responses.append(articles)
-            else:
+            if status != "OK":
                 break
+
         return page_responses
+    
+
+class NYTExtractor:
+    def __init__(self):
+        self.prefix = "https://www.nytimes.com/images/"
+        self.pattern = "^images\/\d{4}\/\d{2}\/\d{2}\/[a-zA-Z]+\/([^\/]+)\/"
+
+    def clean_imgs(self,nyt_art):
+        """
+        Create images list deduplicating images
+        """
+        imgs_list = []
+        img_prefixes = set()
+        multimedia = nyt_art["multimedia"]
+        for item in multimedia:
+            match = re.match(self.pattern,item["url"])
+            if match:
+                img_prefix = match.group()
+            else: 
+                img_prefix = None
+            partial_url = "/" + item["url"]
+            item_url = make_link_absolute(partial_url,self.prefix)
+            #The NYT API response include multiple versions of the same image
+            #We are keeping only the first observation to avoid duplication
+            if img_prefix in img_prefixes:
+                continue
+            else:
+                img_prefixes.add(img_prefix)
+                img_item = Image(url=item_url,caption=item["caption"] or "",
+                      image_type=ImageType("main"))
+                imgs_list.append(img_item)
+        return imgs_list
+
+    def scrape(self,nyt_art):
+        """
+        Convert article in NYT response into Article object
+        """
+        t_text = nyt_art["headline"]["main"]
+        lead_paragraph = nyt_art["lead_paragraph"]
+        imgs_list = self.clean_imgs(nyt_art)
+        return Article(title=t_text,article_text=lead_paragraph,
+                       images=imgs_list)
